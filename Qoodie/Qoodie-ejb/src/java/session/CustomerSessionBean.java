5f7c88cd2b06393fcaa58c30696ad1dbc2e090ce
zhyu1207 <zhy47@126.com>
2018-10-20 21:38:36 +0800

    Update CustomerSessionBean with CRUD

--- Qoodie/Qoodie-ejb/src/java/session/CustomerSessionBean.java
+++ Qoodie/Qoodie-ejb/src/java/session/CustomerSessionBean.java
@@ -5,7 +5,14 @@
  */
 package session;
 
+import entity.Customer;
+import entity.CustomerOrder;
+import error.CustomerNotFoundException;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -13,7 +20,52 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class CustomerSessionBean implements CustomerSessionBeanLocal {
+    @PersistenceContext(unitName = "Qoodie-ejbPU")
+    private EntityManager em;
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @EJB
+    CustomerOrderSessionBean customerOrderSessionBean;
+    @Override
+    public void createCustomer(Customer c) {
+        em.persist(c);
+    }
+
+    @Override
+    public Customer readCustomer(Long cId) throws CustomerNotFoundException {
+        Customer c = em.find(Customer.class, cId);
+        if (c == null) throw new CustomerNotFoundException("customer not found");
+        return c;
+    }
+
+    @Override
+    public void updateCustoemr(Customer c) throws CustomerNotFoundException {
+        Customer oldC = readCustomer(c.getId());
+        oldC.setAddress(c.getAddress());
+        oldC.setCreated(c.getCreated());
+        oldC.setCustomerOrders(c.getCustomerOrders());
+        oldC.setEmail(c.getEmail());
+        oldC.setIsActive(c.getIsActive());
+        oldC.setName(c.getName());
+        oldC.setPassword(c.getPassword());
+        oldC.setPhone(c.getPhone());
+        oldC.setUserType(c.getUserType());
+    }
+
+    @Override
+    public void deleteCustomer(Customer c) throws CustomerNotFoundException {
+        Customer customer = readCustomer(c.getId());
+        List <CustomerOrder> customerOrders = customer.getCustomerOrders();
+        // cascade delete all customer orders associated
+        for (CustomerOrder co : customerOrders){
+            //customerOrderSessionBean.deleteCustomerOrders(co); TODO: implement this method 
+        }
+        em.remove(c);
+    }
+
+    @Override
+    public List<Customer> readAllCustomer() {
+        return em.createQuery("SELECT c FROM Customer c").getResultList();
+    }
+
+    
 }
