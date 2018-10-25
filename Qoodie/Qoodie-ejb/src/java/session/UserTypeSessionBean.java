4d80e941568149e607e26f5d2eeb242205cd986a
zhyu1207 <zhy47@126.com>
2018-10-25 19:21:47 +0800

    Update user type CRUD

--- Qoodie/Qoodie-ejb/src/java/session/UserTypeSessionBean.java
+++ Qoodie/Qoodie-ejb/src/java/session/UserTypeSessionBean.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package session;
 
 import entity.UserType;
@@ -20,23 +20,33 @@ import javax.persistence.PersistenceContext;
 public class UserTypeSessionBean implements UserTypeSessionBeanLocal {
     @PersistenceContext(unitName = "Qoodie-ejbPU")
     private EntityManager em;
-
+    
     @Override
     public void createUserType(UserType u) {
         em.persist(u);
     }
-
+    
     @Override
     public UserType readUserType(Long uId) throws UserTypeNotFoundException {
         UserType u = em.find(UserType.class, uId);
         if (u == null) throw new UserTypeNotFoundException("user type not found");
         return u;
     }
-
+    
+    @Override
+    public void updateUserType(UserType u) throws UserTypeNotFoundException {
+        UserType oldU = readUserType(u.getId());
+        oldU.setName(u.getName());
+    }
+    
+    @Override
+    public void deleteUserType(UserType u) throws UserTypeNotFoundException {
+        em.remove(readUserType(u.getId()));
+    }
+    
     @Override
     public List<UserType> readAllUserType() {
         return em.createQuery("SELECT u from UserType u").getResultList();
     }
-
-  
+    
 }
