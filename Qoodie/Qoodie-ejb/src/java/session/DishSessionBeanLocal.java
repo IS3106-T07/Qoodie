ae84c8419f683f51b8942fbf471192e452123860
zhyu1207 <zhy47@126.com>
2018-10-25 18:53:18 +0800

    Add CRUD of dish bean

--- Qoodie/Qoodie-ejb/src/java/session/DishSessionBeanLocal.java
+++ Qoodie/Qoodie-ejb/src/java/session/DishSessionBeanLocal.java
@@ -5,6 +5,9 @@
  */
 package session;
 
+import entity.Dish;
+import error.DishNotFoundException;
+import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -14,4 +17,10 @@ import javax.ejb.Local;
 @Local
 public interface DishSessionBeanLocal {
     
+    public void createDish(Dish d);
+    public Dish readDish(Long dId) throws DishNotFoundException;
+    public void updateDish(Dish d) throws DishNotFoundException;
+    public void deleteDish(Dish d) throws DishNotFoundException;
+    
+    public List<Dish> readAllDishes();
 }
