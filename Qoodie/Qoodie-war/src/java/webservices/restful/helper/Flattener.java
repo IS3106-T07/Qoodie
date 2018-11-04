5a501ce66f0f2788b8bd11b0515e4944fb1c94c9
zhyu1207 <zhy47@126.com>
2018-11-04 21:32:30 +0800

    Fix flattener null pointer exception

--- Qoodie/Qoodie-war/src/java/webservices/restful/helper/Flattener.java
+++ Qoodie/Qoodie-war/src/java/webservices/restful/helper/Flattener.java
@@ -31,7 +31,7 @@ public class Flattener {
         }
         return c;
     }
-    
+
     public static CustomerOrder flatten(CustomerOrder co) {
         co.getCustomer().setCustomerOrders(null);
         co.getCustomerOrderType().setCustomerOrders(null);
@@ -44,14 +44,19 @@ public class Flattener {
 
     public static Store flatten(Store s) {
         s.getCanteen().setStores(null);
-        s.getCuisineType().setStores(null);
+        if (s.getCuisineType() != null) {
+            s.getCuisineType().setStores(null);
+        }
+        
+
         for (Dish d : s.getDishes()) {
+            d = flatten(d);
             d.setStore(null);
         }
 
         return s;
     }
-    
+
     public static Canteen flatten(Canteen c) {
         for (Store s : c.getStores()) {
             s.setCanteen(null);
@@ -61,51 +66,53 @@ public class Flattener {
         }
         return c;
     }
-    
-    public static CuisineType flatten( CuisineType ct){
-        for (Store s: ct.getStores()){
+
+    public static CuisineType flatten(CuisineType ct) {
+        for (Store s : ct.getStores()) {
             s.setCanteen(null);
         }
         return ct;
     }
-    
-    public static CustomerOrderType flatten(CustomerOrderType ot){
-        for (CustomerOrder co: ot.getCustomerOrders()){
+
+    public static CustomerOrderType flatten(CustomerOrderType ot) {
+        for (CustomerOrder co : ot.getCustomerOrders()) {
             co.setCustomerOrderType(null);
         }
         return ot;
     }
-    
-    public static Dish flatten(Dish d){
-        d.getDishType().setDishes(null);
-        for (OrderDish od: d.getOrderDishes())
+
+    public static Dish flatten(Dish d) {
+        if(d.getDishType() != null)
+            d.getDishType().setDishes(null);
+        for (OrderDish od : d.getOrderDishes()) {
             od.setDish(null);
+        }
         d.getStore().setDishes(null);
         return d;
     }
-    
-    public static DishType flatten(DishType dt){
-        for (Dish d: dt.getDishes()){
+
+    public static DishType flatten(DishType dt) {
+        for (Dish d : dt.getDishes()) {
             d = flatten(d);
             d.setDishType(null);
         }
         return dt;
     }
-    
-    public static OrderDish flatten(OrderDish od){
+
+    public static OrderDish flatten(OrderDish od) {
         od.setCustomerOrder(null);
         od.setDish(flatten(od.getDish()));
         return od;
     }
-    
-    public static UserType flatten (UserType ut){
-        for (Customer c : ut.getCustomers()){
+
+    public static UserType flatten(UserType ut) {
+        for (Customer c : ut.getCustomers()) {
             c.setUserType(null);
-            for (CustomerOrder co: c.getCustomerOrders()){
+            for (CustomerOrder co : c.getCustomerOrders()) {
                 co.setOrderDishes(null);
             }
         }
         return ut;
     }
-    
+
 }
