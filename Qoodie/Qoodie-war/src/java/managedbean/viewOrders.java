914cb07c5fec59e56c7d0c0ae0200c9c1e020c7d
Dipy <subhodipmandal1997@gmail.com>
2018-11-11 13:54:21 +0800

    calendar implemented front end. revenue managedBean can find out the week first day,last day from the input date.

--- Qoodie/Qoodie-war/src/java/managedbean/viewOrders.java
+++ Qoodie/Qoodie-war/src/java/managedbean/viewOrders.java
@@ -3,10 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package managedbean;
 
- package managedbean;
-
-
+import entity.CustomerOrder;
 import entity.OrderDish;
 import javax.inject.Named;
 
@@ -16,8 +15,12 @@ import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import session.CustomerOrderSessionBeanLocal;
 import session.OrderDishSessionBeanLocal;
 
 /**
@@ -29,64 +32,56 @@ import session.OrderDishSessionBeanLocal;
 @ViewScoped
 public class viewOrders implements Serializable {
 
-   private Date date;
-   private boolean showIncoming;
-   private boolean showCompleted;
-   private long storeId;
-   private List<OrderDish> incomingOrders;
-   private List<OrderDish> completed;
-   @EJB
-   OrderDishSessionBeanLocal orderDishSessionLocal;
+    
+    private boolean showIncoming;
+    private boolean showCompleted;
+    private long storeId;
+    private List<OrderDish> incomingOrders;
+    private List<OrderDish> completed;
+    @EJB
+    OrderDishSessionBeanLocal orderDishSessionLocal;
    
     public viewOrders() {
+      
         System.out.println("HERE HERE");
     }
-    
+
     @PostConstruct
     public void init() {
         System.out.println("TES TEST");
-        storeId=18;
+        storeId = 18;
         incomingOrders = new ArrayList<OrderDish>();
         completed = new ArrayList<OrderDish>();
         seperateOrders();
     }
+
     
-    public void seperateOrders()
-    {
-        
+    public void seperateOrders() {
+
         System.out.println("TES BEAN " + orderDishSessionLocal);
         List<OrderDish> orders = orderDishSessionLocal.getStoreOrder(getStoreId());
         System.out.println("retrieved" + orders);
-        for(OrderDish o :orders)
-        {
-         //   System.out.println("order status - "+ o.getCustomerOrder().getCustomerOrderType().getName());
-            if(o.getCustomerOrder().getCustomerOrderType().getName().toLowerCase().equals("paid"))
-            {
+        for (OrderDish o : orders) {
+            //   System.out.println("order status - "+ o.getCustomerOrder().getCustomerOrderType().getName());
+            if (o.getCustomerOrder().getCustomerOrderType().getName().toLowerCase().equals("paid")) {
                 incomingOrders.add(o);
                 System.out.println("Incoming - " + incomingOrders);
             }
-            if(o.getCustomerOrder().getCustomerOrderType().getName().toLowerCase().equals("delivered"))
-                {
-                    completed.add(o);
-                    System.out.println("Completed - " + completed);
-                }
+            if (o.getCustomerOrder().getCustomerOrderType().getName().toLowerCase().equals("delivered")) {
+                completed.add(o);
+                System.out.println("Completed - " + completed);
+            }
         }
     }
-//  
-//    public void onDateSelect(SelectEvent event) {
-//        FacesContext facesContext = FacesContext.getCurrentInstance();
-//        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-//        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+
+//    public double getRevenue(storeId, date, date) {
+//        return customerOrderSessionBean.calculateRevenue(storeId, date, date);
 //    }
-//     
-//    public void click() {
-//      RequestContext requestContext = RequestContext.getCurrentInstance();  
-//      requestContext.update("form:display");  
-//      requestContext.execute("PF('dlg').show()");  
+//
+//    public List<CustomerOrder> getOrders(storeId, date, date) {
+//        return customerOrderSessionBean.orderbyDate(storeId, date, date);
 //    }
-// 
-//    
-    
+
     /**
      * @return the storeId
      */
@@ -101,7 +96,6 @@ public class viewOrders implements Serializable {
         System.out.println("Latest Orders Retrieved");
         seperateOrders();
     }
-    
 
     /**
      * @return the completed
@@ -125,52 +119,31 @@ public class viewOrders implements Serializable {
         this.completed = completed;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    /**
-     * @param date the date to set
-     */
-    public void setDate(Date date) {
-        this.date = date;
-    }
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    /**
+//     * @param date the date to set
+//     */
+//    public void setDate(Date date) {
+//        this.date = date;
+//        System.out.println("after setting date - " + date);
+//    }
 
     /**
      * @return the incomingOrders
      */
     public List<OrderDish> getIncomingOrders() {
-//        updateOrders();
+
         return incomingOrders;
     }
-    
+
     /**
      * @param incoming the incomingOrders to set
      */
     public void setIncomingOrders(List<OrderDish> incomingOrders) {
-        
+
         this.incomingOrders = incomingOrders;
     }
 }
-   /**  public String showIncoming()
-    {
-       setShowIncoming(true);
-        return "index.xhtml";
-    }
-    public String hideIncoming()
-    {
-        setShowIncoming(false);
-        return "index.xhtml";
-    }
-    public String showCompleted()
-    {
-        setShowCompleted(true);
-        return "index.xhtml";
-    }
-    public String hideCompleted()
-    {
-        setShowCompleted(false);
-        return "index.xhtml";
-    }
-    
-**/
