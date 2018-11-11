72f919d9ef6a176483b1a5ecd035b308bdae7cc3
zhyu1207 <zhy47@126.com>
2018-11-11 23:03:40 +0800

    implement endpoint 18

--- Qoodie/Qoodie-war/src/java/webservices/restful/ApplicationConfig.java
+++ Qoodie/Qoodie-war/src/java/webservices/restful/ApplicationConfig.java
@@ -36,7 +36,7 @@ public class ApplicationConfig extends Application {
         resources.add(webservices.restful.CORSFilter.class);
         resources.add(webservices.restful.CanteensResources.class);
         resources.add(webservices.restful.CustomersResource.class);
-        resources.add(webservices.restful.OrderItemResource.class);
+        resources.add(webservices.restful.OrderDishesResource.class);
         resources.add(webservices.restful.StoresResources.class);
     }
     
