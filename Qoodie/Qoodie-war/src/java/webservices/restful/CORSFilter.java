4c96fd9fc678f4f1386fe6a41e505f739e06c97d
zhyu1207 <zhy47@126.com>
2018-11-14 11:55:44 +0800

    update CORS filter

--- Qoodie/Qoodie-war/src/java/webservices/restful/CORSFilter.java
+++ Qoodie/Qoodie-war/src/java/webservices/restful/CORSFilter.java
@@ -20,7 +20,7 @@ public class CORSFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext response) {
         response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
-        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE, PATCH");
         response.getHeaders().putSingle("Access-Control-Allow-Headers", "*");
     }
     
